886e27b39acaa21500acc92d1a5c528b3b8cb8fd
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-06-14 22:09:56 +0530

    added hooks to the form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Form from './Components/Form';
+import TodoList from './Components/TodoList';
 
 
 function App() {
   return (
     <>
-      <header className = "title">My Todo list</header>
+      <header className="title">My Todo list</header>
       <Form />
+      <TodoList />
     </>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,10 +1,36 @@
+import React, {useState} from 'react';
 const Form = () => {
+
+    const [inputText, setInputText] = useState("");  
+    const [todos, settodos] = useState([]);
+
+    const inputTextHandler= (e) => {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        console.log("Hello Detective");
+        settodos([
+            ...todos,
+            {text : inputText, completed : false, id : Math.random() * 1000}
+        ]);
+        setInputText("");
+    };
     return (
         <form>
-            <input type="text" className="enter-todo"></input>
-            <button className="todo-btn" type="submit">
+            <input onChange = {inputTextHandler} value={inputText} type="text" className="enter-todo"></input>
+            <button onClick = {submitTodoHandler} className="todo-btn" type="submit">
                 <i className="fa fa-plus plus-icon"></i>
             </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option vlaue="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="pending">Pending</option>
+                </select>
+            </div>
         </form>
     );
 }
